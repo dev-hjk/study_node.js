1bc4e7b98de45bc93ef83cd512bdb0d2fbd41533
dev-hjk <gpwls7357@naver.com>
2024-07-23 17:26:26 +0900

    컬렉션의 document 전부 출력하는 법, array, object에서 데이터 뽑아내는 법

--- server.js
+++ server.js
@@ -9,21 +9,21 @@ const { MongoClient } = require('mongodb')
 
 //몽고 db 연결 하는 법 
 let db
-const url = 
+const url = 'result[0],title';
 new MongoClient(url).connect().then((client) => {
     console.log('DB연결성공')
     db = client.db('forum')
-    app.listen(8085, () => { 
-        console.log('http://localhost:8085 에서 서버 실행중')
+    app.listen(8080, () => {
+        console.log('http://localhost:8080 에서 서버 실행중')
     })
 }).catch((err) => {
     console.log(err)
 })
 
 //밑 코드 서버 띄울↙️↙️ 포트 번호
-app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행중')
-})
+// app.listen(8080, () => {
+//     console.log('http://localhost:8080 에서 서버 실행중')
+// })
 
 // 밑 코드 해석하면 누가 메인페이지 접속 시 '반갑다'라고 보내달라는 의미!
 //누가 /URL 이런 URL로 접속하면 밑에응답.send('데이터~~~') 에서 데이터~~~ 보내주라는 뜻1
@@ -34,12 +34,12 @@ app.listen(8080, () => {
 app.get('/news', (요청, 응답) => {
     db.collection('post').insertOne({ title: '어쩌구' })
     //db에서 가져오려면 db.collection('db에서의 collection이름')이 코드가 실행되면 insertOne({ title: '어쩌구' })이 데이터가 저장이 된다는 뜻!
-    응답.send('오늘 비옴~~~')
+    // 응답.send('오늘 비옴~~~')
 })
 
 //콜백함수 : 다른 함수 파라미터에 들어가는 함수(밑 코드에서 보면 get함수 밑에 function 함수가 들어가 있음. 이때 function을 콜백함수라고 볼 수 있음!)
 //shop 먼저 실행 후 콜백함수 실행 됨! 
-app.get('/shop', function(요청, 응답){
+app.get('/shop', function (요청, 응답) {
     응답.send('쇼핑페이지임~~~')
 })
 //간단한 서버 기능임 
@@ -49,8 +49,8 @@ app.get('/shop', function(요청, 응답){
 app.get('/', (요청, 응답) => {
     응답.sendFile(__dirname + '/index.html')
 })
-    //위 코드는 경로를 잡아준 것! 
-    //__dirname : 현재 프로젝트 절대 경로!
+//위 코드는 경로를 잡아준 것! 
+//__dirname : 현재 프로젝트 절대 경로!
 //  /index.html : 이렇게 한 이유는 지금 index.html을 어떤 폴더 안에 넣어준게 아니라 루트에 있으니까 앞에 루트라고 해준 것!
 
 //누가 메인페이지 방문시 html 파일 보내라고 코드는 이렇게 짭니다. 
@@ -71,5 +71,14 @@ __dirname 뒤에 / index.html 만 추가하면 index.html 파일경로가 나옵
 app.get('/about', (요청, 응답) => {
     응답.sendFile(__dirname + '/my.html')
 })
+
+app.get('/list', async (요청, 응답) => {
+    let result = await db.collection('post').find().toArray()
+    //컬렉션의 document 전부 출력하는 법
+    console.log(result[0], title)
+    응답.send(result[0], title)
+    //array, object에서 데이터 뽑는 법
+})
+
 
 
